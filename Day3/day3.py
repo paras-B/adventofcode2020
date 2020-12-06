0a0d66e20b06423dd2c1c2cf3230d69ee842f1c3
parasB <parasbhatia999@gmail.com>
2020-12-05 19:43:32 -0500

    adding part 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -8,17 +8,32 @@ def parseFile(filename):
 		data = list(i.rstrip() for i in in_)
 	return data
 
+"""
+Part 1
+"""
 
-def countTrees(list):
-	count = 0
-	start = 0
-	for i in range(1, len(list)):
-		row = data[i]
-		start = (start+3)%len(row)
-		if row[start]=='#':
-			count+=1
-	return count
+def countTrees(list, dc, dr):
+	score = 0
+	r=0
+	c=0
+	while r+1<len(list):
+		c+=dc
+		r+=dr
+		print(c, r)
+		if list[r][c%len(list[r])]=='#':
+			score+=1
+	return score
+
+""" 
+Part 2
+"""
+slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
 if __name__ == '__main__':
 	data = parseFile('input.txt')
-	print(countTrees(data))
+	print(countTrees(data, 3,1))
+
+	ans = 1
+	for (dc,dr) in slopes:
+		ans*=countTrees(data, dc, dr)
+	print(ans)
